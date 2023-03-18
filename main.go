5e4466c4c25bf3a5dc5af6eccd706b642f033672
triet <pvmtriet232@gmail.com>
2023-03-18 15:12:25 +0700

    add main

--- main.go
+++ main.go
@@ -1,32 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pvmtriet232/test"
+)
 
 func main() {
-	lookup := make(map[string]int)
-	lookup["goku"] = 9001
-	power, exists := lookup["goku"]
-	// prints 0, false
-	// 0 is the default value for an integer
-	fmt.Println(power, exists)
-	delete(lookup, "goku")
-	fmt.Println(lookup)
-}
-
-type Person struct {
-	Name string
-}
-
-func (p *Person) Introduce() {
-	fmt.Printf("Hi, I'm %s\n", p.Name)
-}
-
-type Saiyan struct {
-	*Person
-	Power int
-}
-
-type Conqueror struct {
-	*Saiyan
-	ConquerState bool
+	fmt.Println(test.PriceCheck(4343))
 }
